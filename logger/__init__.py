da1bdbf5607b375549e752b3d3cc5e863b242fba
E15dev <85256998+E15dev@users.noreply.github.com>
2021-12-17 23:52:33 +0100

    log level added to logger

--- logger/__init__.py
+++ logger/__init__.py
@@ -23,25 +23,30 @@ cwarning = dark_magenta
 cerror = dark_red
 cfatal = red
 
+version = "2.0"
+
 
 class logger:
     def __init__(self, logdir, realprint):
         global rp, lastlog, thislog
+        # last log
         lln = str(logdir) + 'lastlog.txt'
-        lastlog = open(lln, 'w')
+        lastlog = open(lln, 'w', encoding="utf-8")
         lastlog.write(str(time.time()) + '\n')
         lastlog.close()
-        lastlog = open(lln, 'a')
+        lastlog = open(lln, 'a', encoding="utf-8")
+        # this log
         tln = str(logdir) + str(time.time()) + '.txt'
-        thislog = open(tln, 'w')
+        thislog = open(tln, 'w', encoding="utf-8")
         thislog.write(str(time.time()) + '\n')
         thislog.close()
-        thislog = open(tln, 'a')
+        thislog = open(tln, 'a', encoding="utf-8")
         rp = realprint
+        self.info("logging with logger version : {0]".format(version))
 
-    def log(self, c, text):
+    def log(self, c, text, level):
         global rp, lastlog, thislog
-        t = str(time.time()) + ' | ' + c + str(text) + '\n'
+        t = str(time.time()) + ' | ' + level + ' | ' + c + str(text) + '\n'
         if rp:
             print(t, end='')
         lastlog.write(t)
@@ -50,20 +55,20 @@ class logger:
         thislog.flush()
         return 0
 
-    def info(self, text):
-        self.log(cinfo, str(text))
+    def info(self, text, ):
+        self.log(cinfo, str(text), level="info")
         return 0
 
     def warning(self, text):
-        self.log(cwarning, str(text))
+        self.log(cwarning, str(text), level="warning")
         return 0
 
     def error(self, text):
-        self.log(cerror, str(text))
+        self.log(cerror, str(text), level="error")
         return 0
 
     def fatal(self, text):
-        self.log(cfatal, str(text))
+        self.log(cfatal, str(text), level="fatal")
         return 0
 
     def end(self, exitcode):
